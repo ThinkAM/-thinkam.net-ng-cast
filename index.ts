904ba981ebde360cee1cf9df1915c24170813a9a
Felipe de Almeida <felipe.almeida@thinkam.net>
2020-11-07 22:11:43 -0300

    time progress style

--- index.ts
+++ index.ts
@@ -2,4 +2,4 @@ export * from './ng.cast.module';
 export * from './shared/ng-cast.service';
 
         // ------ project path -------- | --- lib ---
-import "/home/thinker/dev/thinkam/-thinkam.net-ng-cast/cast-videos.js";
+import "~/felipe/dev/GitHub/ThinkAM/-thinkam.net-ng-cast/cast-videos.js";
